20ae5a8ec20e4488d821cee7dc933cfac4e8d0ca
Viktoria-Jikaeva <131869303+Viktoria-Jikaeva@users.noreply.github.com>
2024-04-09 13:48:55 +0300

    Initial Commit

---
+++ FilmsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmsApp
+//
+//  Created by Виктория on 09.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
